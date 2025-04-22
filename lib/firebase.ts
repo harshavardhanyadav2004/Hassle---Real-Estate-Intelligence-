7a7f1845af24c01a419604c622bd8c7961a0350d
harshavardhanyadav2004 <21981a4466@raghuenggcollege.in>
2025-04-22 16:32:41 +0530

    Updated code

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDIutHUc-ujomADfztUXVeBHhG-tMBdUDw",
-    authDomain: "hae-78d29.firebaseapp.com",
-    projectId: "hae-78d29",
-    storageBucket: "hae-78d29.firebasestorage.app",
-    messagingSenderId: "135927361386",
-    appId: "1:135927361386:web:de7db0121afd4fb3c4bf13",
-    measurementId: "G-LX28S4JYK4",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase app only once
