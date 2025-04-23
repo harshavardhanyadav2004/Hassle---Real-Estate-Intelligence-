a9b95faa79a734e273fd3be87ef161fbb5ffb289
Boora sri Harsha Vardhan <138381780+harshavardhanyadav2004@users.noreply.github.com>
2025-04-23 21:37:41 +0530

    Update chat-interface.tsx

--- components/chat-interface.tsx
+++ components/chat-interface.tsx
@@ -190,30 +190,7 @@ export default function ChatInterface({
       if (!response.ok) {
         throw new Error("Failed to send message")
       }
-      const allowCors = fn => async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true)
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        // another common pattern
-        // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
-        if (req.method === 'OPTIONS') {
-          res.status(200).end()
-          return
-        }
-        return await fn(req, res)
-      }
-
-      const handler = (req, res) => {
-        const d = new Date()
-        res.end(d.toString())
-      }
-
-      module.exports = allowCors(handler)
-
+      
 
       const data = await response.json()
       const responseMessage: ChatMessage = {
